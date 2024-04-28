e539f25762f9989f05915b2a4816344a202fcf2e
bilal benkaddi <bilalebenkaddi203@gmail.com>
2024-04-28 03:19:49 +0200

    new entreprise controller

--- backend/app/Http/Controllers/CandidatureController.php
+++ backend/app/Http/Controllers/CandidatureController.php
@@ -8,18 +8,23 @@ use Illuminate\Http\Request;
 
 class CandidatureController extends Controller
 {
-    public function index():Collection
+    public function index(): Collection
     {
         return Candidature::all();;
     }
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'offre_emploi_id',
+            'candidat_id',
+            'cv',
+            'lettre_de_motivation',
+        ]);
     }
 
     public function show(Candidature $candidature)
     {
-        return $candidature->load("offre","candidat") ;
+        return $candidature->load("offre", "candidat");
     }
     public function update(Request $request, Candidature $candidature)
     {
--- backend/app/Http/Controllers/EntrepriseController.php
+++ backend/app/Http/Controllers/EntrepriseController.php
@@ -8,24 +8,54 @@ use Illuminate\Http\Request;
 
 class EntrepriseController extends Controller
 {
-    public function index() : Collection
+    public function index(): Collection
     {
         return Entreprise::all();
     }
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'nom_Entreprise' => "required|min:3|max:120",
+            'secteur' => "required|min:3|max:120",
+            'adresse' => "required|min:3|max:250",
+            'code_postal' => "required|min:2|max:18|numeric",
+            'ville' => "required|min:3|max:50",
+            'Pays' => "required|min:3|max:120",
+            'telephone' => "required|min:10|max:13",
+            'email' => "required|min:3|max:120",
+            'password' => "required|min:8|max:18",
+            'site_web' => "min:3|max:120",
+        ]);
+
+        return Entreprise::create($validated);
     }
     public function show(Entreprise $entreprise)
     {
-        return $entreprise ;
+        return $entreprise;
     }
     public function update(Request $request, Entreprise $entreprise)
     {
-        //
+        $validated = $request->validate([
+            'nom_Entreprise' => "required|min:3|max:120",
+            'secteur' => "required|min:3|max:120",
+            'adresse' => "required|min:3|max:250",
+            'code_postal' => "required|min:2|max:18|numeric",
+            'ville' => "required|min:3|max:50",
+            'Pays' => "required|min:3|max:120",
+            'telephone' => "required|min:10|max:13",
+            'email' => "required|min:3|max:120",
+            'password' => "required|min:8|max:18",
+            'site_web' => "min:3|max:120",
+        ]);
+        $entreprise->update($validated);
+
+        return redirect()->route("candidats.index")->with('success', 'Entreprise updated successfully');
+        
     }
     public function destroy(Entreprise $entreprise)
     {
-        //
+        $entreprise->delete();
+        return redirect()->route("candidats.index")->with('success', 'Entreprise deleted successfully');
+
     }
 }
